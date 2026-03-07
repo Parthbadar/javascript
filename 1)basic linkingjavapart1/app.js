7515a2ec7fac9b0acac68860df3b276a565b0be5
Parthbadar <parthbadar9@gmail.com>
2026-03-07 14:31:18 +0530

    initial

---
+++ 1)basic linkingjavapart1/app.js
@@ -0,0 +1,30 @@
+// console.log("hello world");
+// console.log("apna college");
+let a=10;
+let b=5;
+console.log("sum is :",a+b);
+
+//Template literals cha part ahe 
+let pencilprice = 10;
+let erasorprice = 5;
+//console.log("the total price is :",pencilprice + erasorprice ,"Rupees");
+console.log(`the total price is : ${pencilprice + erasorprice} Rupees`);
+
+// operators in js 
+// 1.Arithmetic operators
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y); // multi sathi ani div,etc.
+
+//unary operators
+console.log(a++);
+console.log(++a);
+
+//assignment
+b=a;
+console.log(b);
+
+//comparison 
+let age = 18
+console.log(age<=19);
