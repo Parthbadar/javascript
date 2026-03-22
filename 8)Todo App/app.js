f14d9c06a955b954770e299d9eb24a52c7b72bfc
Parthbadar <parthbadar9@gmail.com>
2026-03-22 18:02:54 +0530

    small project for learning

---
+++ 8)Todo App/app.js
@@ -0,0 +1,31 @@
+let todo = []
+let req = prompt("pls enter your request");
+
+while(true){
+    if(req == "quit"){
+        console.log("quitting app");
+        break;
+    }
+    else if(req == "list"){
+        console.log("----------------");
+        for(let i=0;i<=todo.length;i++){
+            console.log(i,todo[i]);
+        }
+        console.log("----------------");
+    }
+    else if(req == "add"){
+        let task = prompt("pls enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    }
+    else if(req == "delete"){
+        let idx = prompt("pls enter the task index");
+        todo.splice(idx,1);
+        console.log("task deleted")
+    }
+    else{
+        console.log("wrong request");
+    }
+    
+    req = prompt("pls enter your request");
+}
