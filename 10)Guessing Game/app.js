4b9156b6e33e10cafc8fd5fe90a862230910ccd6
Parthbadar <parthbadar9@gmail.com>
2026-04-16 21:13:04 +0530

    game

--- 10)Guessing Game/app.js
+++ 10)Guessing Game/app.js
@@ -1,1 +1,21 @@
 const max = prompt("enter the max number");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("guess the number");
+
+while(true) {
+    if(guess == "quit"){
+        console.log("user quit");
+        break;
+    }
+    if(guess == random){
+        console.log("you are right! congrats!");
+        break;
+    } else if(guess < random){
+        guess = prompt("hint: your guess was too small . pls try ");
+
+    }else {
+        guess = prompt("hint: your guess was too large . pls try ");
+}
+}
